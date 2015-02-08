587d298ad28eb4fe421a0470625ba3afc309d073
koke6645 <koke6645@gmail.com>
2015-02-08 17:08:44 -0600

    init

---
+++ CAT_AudioTour/PassData.swift
@@ -0,0 +1,13 @@
+//
+//  PassData.swift
+//  CAT_AudioTour
+//
+//  Created by Red_iMac on 2/8/15.
+//  Copyright (c) 2015 Red_iMac. All rights reserved.
+//
+
+import Foundation
+struct PassData {
+    var id : Int
+    var title : String
+}
